a61672c7bb84338451054469670873b7af3bcbc1
24111 <shadowseeker714@gmail.com>
2022-11-07 22:13:19 -0800

    Update better_storage.meta.js

--- better_storage/better_storage.meta.js
+++ better_storage/better_storage.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DS - Better Storage
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  try to take over the world!
 // @author       XHunter
 // @updateURL    https://raw.githubusercontent.com/LN-24111/dropshock-scripts/main/better_storage/better_storage.meta.js
