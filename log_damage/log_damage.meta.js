be0a1c7928942258621fa270740213cbc9e75d19
24111 <shadowseeker714@gmail.com>
2022-11-16 19:42:52 -0800

    Create log_damage.meta.js

---
+++ log_damage/log_damage.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         DS - Log Damage
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  try to take over the world!
+// @author       XHunter
+// @updateURL    https://raw.githubusercontent.com/LN-24111/dropshock-scripts/main/log_damage/log_damage.meta.js
+// @downloadURL  https://raw.githubusercontent.com/LN-24111/dropshock-scripts/main/log_damage/log_damage.js
+// @match        *://command.drop-shock.com/report.php*
+// @require      https://code.jquery.com/jquery-3.4.1.min.js
+// @grant        none
+// ==/UserScript==
