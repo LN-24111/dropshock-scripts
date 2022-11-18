3be6b5b39c857c6d61529686497ec3ff8f436055
24111 <shadowseeker714@gmail.com>
2022-11-18 02:00:15 -0800

    Update better_storage.meta.js
    Changed hotkey to E: Expand, X: select "bandits" (no variance) units for scrapping

--- better_storage/better_storage.meta.js
+++ better_storage/better_storage.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DS - Better Storage
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  try to take over the world!
 // @author       XHunter
 // @updateURL    https://raw.githubusercontent.com/LN-24111/dropshock-scripts/main/better_storage/better_storage.meta.js
